234feccea2158cd574f1f7e9049407c019426c37
morisset <chris.morisset@gmail.com>
2015-12-16 17:59:25 -0600

    Adding a test_get_volume for some bug search.

---
+++ test_get_volume.py
@@ -0,0 +1,19 @@
+'''
+Created on 16 déc. 2015
+
+@author: christophemorisset
+'''
+import ads
+
+bibcode = '2014ApJ...784..173D'
+res = ads.SearchQuery(q='citations(bibcode:{})'.format(bibcode), fl='author, title, year, pub, volume, page, bibcode')
+papers = list(res)
+
+# the following does not connect to ADS
+for p in papers:
+    print p.title
+    
+# the following DOES connect to ADS, because two papers have None as volume.
+for p in papers:
+    print p.volume
+
